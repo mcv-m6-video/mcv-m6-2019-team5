cbb7340c06fd8afd72f6e0521776099001d5f733
albamhp <albaherrerapalacio@gmail.com>
2019-03-01 00:15:50 +0100

    mAP

--- src/metrics/mean_average_precision.py
+++ src/metrics/mean_average_precision.py
@@ -1,8 +1,16 @@
 from typing import List
-
 from model import Frame
+from functional import seq
+import numpy as np
 
 
 def mean_average_precision(frames: List[Frame]) -> float:
-    # TODO
-    pass
+    ap = [0 for _ in range(11)]
+    recalls = seq(frames).map(lambda f: f.get_recall).to_list()
+    precisions = seq(frames).map(lambda f: f.get_precision).to_list()
+    for precision, recall in zip(precisions, recalls):
+        index = int(recall * 10)
+        if precision > ap[index]:
+            ap[index] = precision
+
+    return float(np.mean(ap))

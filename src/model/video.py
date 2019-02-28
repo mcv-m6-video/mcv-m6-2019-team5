3f667a87a44f484450058f04ee754771f3c5162c
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-02-28 23:25:27 +0100

    Removed ffmpeg

--- src/model/video.py
+++ src/model/video.py
@@ -1,30 +1,23 @@
-from typing import List
 # from model import Frame
-import ffmpeg
-import numpy as np
+import cv2
+from xml.dom.minidom import parse, parseString
 
 
 class Video:
     # frames: List[Frame]
 
     def __init__(self, video_path: str, annotation_path: str):
-        probe = ffmpeg.probe(video_path)
-        video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-        width = int(video_stream['width'])
-        height = int(video_stream['height'])
-        out, _ = (
-            ffmpeg
-                .input(video_path)
-                .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-                .run(capture_stdout=True)
-        )
-        video = (
-            np
-                .frombuffer(out, np.uint8)
-                .reshape([-1, height, width, 3])
-        )
-        print(video.shape)
+        cap = cv2.VideoCapture(video_path)
+        while cap.isOpened():
+            ret, frame = cap.read()
 
+            # TODO create frame list
+
+            cv2.imshow('frame', frame)
+
+        cap.release()
+        cv2.destroyAllWindows()
+        # TODO
 
 
 video = Video("../../datasets/AICity_data/train/S03/c010/vdo.avi", "")

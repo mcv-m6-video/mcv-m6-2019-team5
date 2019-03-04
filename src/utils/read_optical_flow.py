e7de78e44a6b97674840f8987d216057b65f11af
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-03-04 23:15:48 +0100

    Fixed optical flow arrows

--- src/utils/read_optical_flow.py
+++ src/utils/read_optical_flow.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def read_optical_flow(path: str):
     im = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    im = np.flip(im, axis=2).astype(np.uint16)
+    im = np.flip(im, axis=2).astype(np.double)
 
-    f_u = (im[:, :, 0] - 2 ^ 15) / 64
-    f_v = (im[:, :, 1] - 2 ^ 15) / 64
+    f_u = (im[:, :, 0] - pow(2, 15)) / 64
+    f_v = (im[:, :, 1] - pow(2, 15)) / 64
     f_valid = im[:, :, 2]
     f_valid[f_valid > 1] = 1
     f_u[f_valid == 0] = 0

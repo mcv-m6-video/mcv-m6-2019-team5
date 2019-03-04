2566cc7e8deacdd7ab409cbf01fa1fb1e7dcd411
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-03-04 13:14:14 +0100

    Fixing read optical flow

--- src/utils/read_optical_flow.py
+++ src/utils/read_optical_flow.py
@@ -3,15 +3,15 @@ import numpy as np
 
 
 def read_optical_flow(path: str):
-    I = cv2.imread('../datasets/optical_flow/detection/LKflow_000045_10.png')
+    im = cv2.imread(path)
 
-    F_u = (I[:, :, 0] - 2 ^ 15) / 64
-    F_v = (I[:, :, 1] - 2 ^ 15) / 64
-    F_valid = I[:, :, 2]
-    F_valid[F_valid > 1] = 1
-    F_u[F_valid == 0] = 0
-    F_v[F_valid == 0] = 0
+    f_u = (im[:, :, 0] - 2 ^ 15) / 64
+    f_v = (im[:, :, 1] - 2 ^ 15) / 64
+    f_valid = im[:, :, 2]
+    f_valid[f_valid > 1] = 1
+    f_u[f_valid == 0] = 0
+    f_v[f_valid == 0] = 0
 
     return np.dstack([
-        F_u, F_v, F_valid
+        f_u, f_v, f_valid
     ])

64d36c60741d385fe5a66a7286f3f8dfcfaaab4e
Jorge Lopez <jorgelopezfueyo@gmail.com>
2019-03-08 18:34:22 +0100

    Optimized gaussian_model and added progress

--- src/methods/week2.py
+++ src/methods/week2.py
@@ -8,7 +8,7 @@ from operations.morphological_operations import closing, opening
 
 def week2(video: Video) -> List[List[Rectangle]]:
     bounding_boxes: List[List[Rectangle]] = []
-    for mask in gaussian_model(video, int(2141 * 0.25)):
+    for mask in gaussian_model(video, int(2141 * 0.25), total_frames=2141):
         mask = opening(closing(mask, 5), 5)
         bbs = find_boxes(mask)
         bounding_boxes.append(bbs)
--- src/operations/gaussian_model.py
+++ src/operations/gaussian_model.py
@@ -1,32 +1,39 @@
+import sys
 from typing import Iterator
 
 import numpy as np
-import matplotlib.pyplot as mp
+from tqdm import tqdm
+
 from model import Video
 
 
-def gaussian_model(video: Video, train_stop_frame: int) -> Iterator[np.ndarray]:
-    for im, frame in video.get_frames():
-        if frame.id == 0:
-            #backgroundMean = np.zeros((im.shape[0], im.shape[1]))
-            backgroundList = np.zeros((im.shape[0], im.shape[1], train_stop_frame))
+def gaussian_model(video: Video, train_stop_frame: int, total_frames: int = None) -> Iterator[np.ndarray]:
+    background_list = None
+    background_mean = None
+    background_std = None
+
+    for im, frame in tqdm(video.get_frames(), total=total_frames, file=sys.stdout):
+        if background_list is None:
+            # backgroundMean = np.zeros((im.shape[0], im.shape[1]))
+            background_list = np.zeros((im.shape[0], im.shape[1], train_stop_frame), dtype=np.int16)
+
         if frame.id < train_stop_frame:
-            backgroundList[:, :, frame.id] = np.mean(im, axis=-1)/255
-            #backgroundMean = np.add(backgroundMean + im.mean(axis=-1, keepdims=0))
-            if train_stop_frame-1 == frame.id:
-                backgroundMean = np.mean(backgroundList, axis=-1)
-                backgroundStd = np.std(backgroundList, axis=1)
+            background_list[:, :, frame.id] = np.mean(im, axis=-1, dtype=np.int16)
+            # backgroundMean = np.add(backgroundMean + im.mean(axis=-1, keepdims=0))
+            if train_stop_frame - 1 == frame.id:
+                background_mean = np.mean(background_list, axis=-1) / 255
+                background_std = np.std(background_list, axis=-1) / 255
 
         else:
-            thershold=0.5
+            threshold = 0.5
             mask = np.zeros((im.shape[0], im.shape[1]))
-            imGray = np.mean(im, axis=-1)/255
+            im_gray = np.mean(im, axis=-1) / 255
             row = 0
-            for rows in imGray:
+            for rows in im_gray:
                 col = 0
                 for pix in rows:
-                    if (abs(pix)-backgroundMean[row,col])>= (thershold * (backgroundStd[row,col]+2)):
+                    if (abs(pix) - background_mean[row, col]) >= (threshold * (background_std[row, col] + 2)):
                         mask[row, col] = 1
-                    col +=1
-                row +=1
+                    col += 1
+                row += 1
             yield mask

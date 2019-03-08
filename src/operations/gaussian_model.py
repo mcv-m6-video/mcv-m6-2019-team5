a53bff5cd413546e4933393804b7b7415af087f3
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-03-08 18:19:13 +0100

    FIX GMM Masks

--- src/operations/gaussian_model.py
+++ src/operations/gaussian_model.py
@@ -6,23 +6,27 @@ from model import Video
 
 
 def gaussian_model(video: Video, train_stop_frame: int) -> Iterator[np.ndarray]:
-
     for im, frame in video.get_frames():
         if frame.id == 0:
-            backgroundMean = np.zeros((im.shape[0], im.shape[1]))
+            #backgroundMean = np.zeros((im.shape[0], im.shape[1]))
+            backgroundList = np.zeros((im.shape[0], im.shape[1], train_stop_frame))
         if frame.id < train_stop_frame:
-            imGray = im.mean(axis=-1, keepdims=0)
-            col = 0
-            for pixRow in imGray:
-                row = 0
-                for pix in pixRow:
-                    meanValue = (backgroundMean[col, row] + pix) / ((row+1)*(col+1))
-                    backgroundMean[col, row] = meanValue
-                    row =+1
-                col =+1
-            pass
-            backgroundMean
+            backgroundList[:, :, frame.id] = np.mean(im, axis=-1)/255
+            #backgroundMean = np.add(backgroundMean + im.mean(axis=-1, keepdims=0))
+            if train_stop_frame-1 == frame.id:
+                backgroundMean = np.mean(backgroundList, axis=-1)
+                backgroundStd = np.std(backgroundList, axis=1)
+
         else:
-            # TODO compute mask and return
-            mask = frame
+            thershold=0.5
+            mask = np.zeros((im.shape[0], im.shape[1]))
+            imGray = np.mean(im, axis=-1)/255
+            row = 0
+            for rows in imGray:
+                col = 0
+                for pix in rows:
+                    if (abs(pix)-backgroundMean[row,col])>= (thershold * (backgroundStd[row,col]+2)):
+                        mask[row, col] = 1
+                    col +=1
+                row +=1
             yield mask

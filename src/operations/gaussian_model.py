31727d5b58291ffd4e4e02fbd041cb319ea9c226
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-03-08 18:48:08 +0100

    MOD two step background model

--- src/operations/gaussian_model.py
+++ src/operations/gaussian_model.py
@@ -7,24 +7,9 @@ from tqdm import tqdm
 from model import Video
 
 
-def gaussian_model(video: Video, train_stop_frame: int, total_frames: int = None) -> Iterator[np.ndarray]:
-    background_list = None
-    background_mean = None
-    background_std = None
-
+def gaussian_model(video: Video, train_stop_frame: int, background_mean: np.ndarray ,background_std: np.ndarray , total_frames: int = None) -> Iterator[np.ndarray]:
     for im, frame in tqdm(video.get_frames(), total=total_frames, file=sys.stdout):
-        if background_list is None:
-            # backgroundMean = np.zeros((im.shape[0], im.shape[1]))
-            background_list = np.zeros((im.shape[0], im.shape[1], train_stop_frame), dtype=np.int16)
-
-        if frame.id < train_stop_frame:
-            background_list[:, :, frame.id] = np.mean(im, axis=-1, dtype=np.int16)
-            # backgroundMean = np.add(backgroundMean + im.mean(axis=-1, keepdims=0))
-            if train_stop_frame - 1 == frame.id:
-                background_mean = np.mean(background_list, axis=-1) / 255
-                background_std = np.std(background_list, axis=-1) / 255
-
-        else:
+        if frame.id >= train_stop_frame:
             threshold = 0.5
             mask = np.zeros((im.shape[0], im.shape[1]))
             im_gray = np.mean(im, axis=-1) / 255
@@ -37,3 +22,17 @@ def gaussian_model(video: Video, train_stop_frame: int, total_frames: int = None
                     col += 1
                 row += 1
             yield mask
+
+def get_background_model (video: Video, train_stop_frame: int, total_frames: int = None)-> (np.ndarray, np.ndarray):
+
+    background_list = None
+    for im, frame in tqdm(video.get_frames(), total=total_frames, file=sys.stdout):
+        if background_list is None:
+            background_list = np.zeros((im.shape[0], im.shape[1], train_stop_frame), dtype=np.int16)
+
+        background_list[:, :, frame.id] = np.mean(im, axis=-1, dtype=np.int16)
+
+    background_mean = np.mean(background_list, axis=-1) / 255
+    background_std = np.std(background_list, axis=-1) / 255
+
+    return background_mean, background_std

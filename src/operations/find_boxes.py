c9b4692838a395ee00d6d098e7831f0993731d6a
albamhp <albaherrerapalacio@gmail.com>
2019-03-12 19:32:33 +0100

    nose

--- src/operations/find_boxes.py
+++ src/operations/find_boxes.py
@@ -7,9 +7,10 @@ from operations.clear_non_region_mask import clear_non_region_mask
 from .get_cc_regions import get_cc_regions
 from .combine_overlapped_regions import combine_overlapped_regions
 
-MIN_AREA = 5000
+MIN_AREA = 10000
 MIN_ASPECT_RATIO = 0.3
 SCALE = 0.15
+MAX_AREA = 250000
 
 def find_boxes(mask: np.ndarray) -> (np.ndarray, List[Rectangle]):
     detections = get_cc_regions(mask)
@@ -28,7 +29,7 @@ def remove_by_shape(detections):
     i = 0
     while i < len(detections):
         detection = detections[i]
-        if detection.get_area() < MIN_AREA or detection.width / detection.height < MIN_ASPECT_RATIO or \
+        if detection.get_area() < MIN_AREA or detection.get_area() > MAX_AREA or detection.width / detection.height < MIN_ASPECT_RATIO or \
                 detection.height / detection.width < MIN_ASPECT_RATIO:
             detections.pop(i)
         else:

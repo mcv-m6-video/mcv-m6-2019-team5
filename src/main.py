494b2af7614df17ec76fcb442509b8dfeeed46d1
albamhp <albaherrerapalacio@gmail.com>
2019-03-12 20:59:00 +0100

    Fixes

--- src/main.py
+++ src/main.py
@@ -37,48 +37,48 @@ def main():
     for im, mask, frame in method(video, **{'debug': args.debug}):
         frames.append(frame)
         iou = frame.get_detection_iou(ignore_classes=True)
+        if not args.debug:
+            plt.figure()
+            plt.subplot2grid((2, 2), (0, 0))
 
-        plt.figure()
-        plt.subplot2grid((2, 2), (0, 0))
+            im1 = np.copy(im)
+            for d in frame.ground_truth:
+                cv2.rectangle(im1, (int(d.top_left[1]), int(d.top_left[0])),
+                              (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (255, 0, 0), thickness=5)
+            for d in frame.detections:
+                cv2.rectangle(im1, (int(d.top_left[1]), int(d.top_left[0])),
+                              (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (0, 0, 255), thickness=5)
+            plt.imshow(cv2.cvtColor(im1, cv2.COLOR_BGR2RGB))
+            plt.axis('off')
+            plt.legend([
+                Line2D([0], [0], color=(0, 0, 1)),
+                Line2D([0], [0], color=(1, 0, 0)),
+            ], ['Ground truth', 'Detection'])
 
-        im1 = np.copy(im)
-        for d in frame.ground_truth:
-            cv2.rectangle(im1, (int(d.top_left[1]), int(d.top_left[0])),
-                          (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (255, 0, 0), thickness=5)
-        for d in frame.detections:
-            cv2.rectangle(im1, (int(d.top_left[1]), int(d.top_left[0])),
-                          (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (0, 0, 255), thickness=5)
-        plt.imshow(cv2.cvtColor(im1, cv2.COLOR_BGR2RGB))
-        plt.axis('off')
-        plt.legend([
-            Line2D([0], [0], color=(0, 0, 1)),
-            Line2D([0], [0], color=(1, 0, 0)),
-        ], ['Ground truth', 'Detection'])
+            plt.subplot2grid((2, 2), (0, 1))
+            m = cv2.cvtColor(np.copy(mask), cv2.COLOR_GRAY2RGB)
+            for d in frame.ground_truth:
+                cv2.rectangle(m, (int(d.top_left[1]), int(d.top_left[0])),
+                              (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (0, 0, 255), thickness=5)
+            for d in frame.detections:
+                cv2.rectangle(m, (int(d.top_left[1]), int(d.top_left[0])),
+                              (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (255, 0, 0), thickness=5)
+            plt.imshow(m)
+            plt.axis('off')
+            plt.legend([
+                Line2D([0], [0], color=(0, 0, 1)),
+                Line2D([0], [0], color=(1, 0, 0)),
+            ], ['Ground truth', 'Detection'])
 
-        plt.subplot2grid((2, 2), (0, 1))
-        m = cv2.cvtColor(np.copy(mask), cv2.COLOR_GRAY2RGB)
-        for d in frame.ground_truth:
-            cv2.rectangle(m, (int(d.top_left[1]), int(d.top_left[0])),
-                          (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (0, 0, 255), thickness=5)
-        for d in frame.detections:
-            cv2.rectangle(m, (int(d.top_left[1]), int(d.top_left[0])),
-                          (int(d.get_bottom_right()[1]), int(d.get_bottom_right()[0])), (255, 0, 0), thickness=5)
-        plt.imshow(m)
-        plt.axis('off')
-        plt.legend([
-            Line2D([0], [0], color=(0, 0, 1)),
-            Line2D([0], [0], color=(1, 0, 0)),
-        ], ['Ground truth', 'Detection'])
+            plt.subplot2grid((2, 2), (1, 0), colspan=2)
+            plt.title('IoU over time')
+            iou_over_time(frames, ignore_classes=True, show=False)
+            axes = plt.gca()
+            axes.set_xlim((0, int(2041*.75) + 1))
+            axes.set_ylim((0, 1.1))
+            #plt.legend()
 
-        plt.subplot2grid((2, 2), (1, 0), colspan=2)
-        plt.title('IoU over time')
-        iou_over_time(frames, ignore_classes=True, show=False)
-        axes = plt.gca()
-        axes.set_xlim((0, int(2041*.75) + 1))
-        axes.set_ylim((0, 1.1))
-        plt.legend()
-
-        plt.show()
+            plt.show()
 
     iou_over_time(frames, ignore_classes=True)
     print('mAP:', mean_average_precision(frames, ignore_classes=True))

9026be4db29926a1d427e8df34f8f09a3d21d56f
Jorge Lopez <jorgelopezfueyo@gmail.com>
2019-03-17 23:52:35 +0100

    Changed to YOLOv3

--- src/methods/off_the_shelf.py
+++ src/methods/off_the_shelf.py
@@ -1,12 +1,18 @@
-import torch
-from faster_rcnn.model.faster_rcnn.resnet import resnet
-
 from model import Video
+from yolo.models import Darknet
+import os
+import wget
 
 
 def off_the_shelf(video: Video):
-    config_file = "configs/e2e_faster_rcnn_R_50_FPN_1x.yaml"
+    model = Darknet('../config/yolov3.cfg')
+    if not os.path.exists('../.cache/yolov3.weights'):
+        if not os.path.exists('../.cache'):
+            os.makedirs('../.cache/')
+        print('Downloading weights...')
+        wget.download('https://pjreddie.com/media/files/yolov3.weights', out='../.cache/')
+        print('Weights downloaded')
 
-    model = resnet([1, 2, 3], 152, pretrained=True).cuda()
+    model.load_weights('../.cache/yolov3.weights')
 
     print(model)

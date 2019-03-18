986033409f435034608e75ad1f0393882ff3ef56
albamhp <albaherrerapalacio@gmail.com>
2019-03-18 20:31:34 +0100

    Minor changes

--- src/methods/off_the_shelf.py
+++ src/methods/off_the_shelf.py
@@ -1,13 +1,8 @@
-import cv2
-
 import torch
-from torchvision.transforms import functional
 from torchvision import transforms
 
 from model import Video
 from yolo.models import Darknet
-import os
-import wget
 
 
 def off_the_shelf():
@@ -17,13 +12,6 @@ def off_the_shelf():
     ]))
 
     model = Darknet('../config/yolov3.cfg')
-    if not os.path.exists('../.weights/yolov3.weights'):
-        if not os.path.exists('../.weights'):
-            os.makedirs('../.weights/')
-        print('Downloading weights...')
-        wget.download('https://pjreddie.com/media/files/yolov3.weights', out='../.weights/')
-        print('Weights downloaded')
-
     model.load_weights('../.weights/yolov3.weights')
     model = model.cuda()
 

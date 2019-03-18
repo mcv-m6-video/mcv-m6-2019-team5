945d70b46d9ce7ae75e3d71c08c322a04107e6e9
albamhp <albaherrerapalacio@gmail.com>
2019-03-18 16:56:05 +0100

    UPDATE detection ids

--- src/operations/kalman_tracking.py
+++ src/operations/kalman_tracking.py
@@ -1,4 +1,4 @@
-from model import Frame
+from model import Frame, Detection
 from operations import Sort
 import numpy as np
 
@@ -9,8 +9,12 @@ class KalmanTracking:
         self.mot_tracker = Sort()  # create instance of the SORT tracker
 
     def __call__(self, frame: Frame):
-        dets = frame.get_format_detections()
-        dets = np.array(dets)
-        trackers = self.mot_tracker.update(dets)
+        detections = frame.get_format_detections()
+        detections = np.array(detections)
+        trackers = self.mot_tracker.update(detections)
+        for det in frame.detections:
+            for track in trackers:
+                if det.top_left[0] == track[0] and det.top_left[1] == track[1]:
+                    det.id = track[4]
 
-        return trackers
+

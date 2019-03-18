11a58adabff639eb9b61554ef8458d2978173430
albamhp <albaherrerapalacio@gmail.com>
2019-03-18 19:56:48 +0100

    Yolo almost working

--- src/methods/off_the_shelf.py
+++ src/methods/off_the_shelf.py
@@ -1,10 +1,21 @@
+import cv2
+
+import torch
+from torchvision.transforms import functional
+from torchvision import transforms
+
 from model import Video
 from yolo.models import Darknet
 import os
 import wget
 
 
-def off_the_shelf(video: Video):
+def off_the_shelf():
+    video = Video("../datasets/AICity_data/train/S03/c010/frames", True, transforms.Compose([
+        transforms.Resize(416),
+        transforms.ToTensor()
+    ]))
+
     model = Darknet('../config/yolov3.cfg')
     if not os.path.exists('../.cache/yolov3.weights'):
         if not os.path.exists('../.cache'):
@@ -15,4 +26,10 @@ def off_the_shelf(video: Video):
 
     model.load_weights('../.cache/yolov3.weights')
 
-    print(model)
+    model.eval()
+    with torch.no_grad():
+        for im in video.get_frames():
+            im = im.view((-1,) + im.size())
+            print(im.size())
+            output = model.forward(im)
+            print(output)

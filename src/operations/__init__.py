017e4337cb3383723491df34eb3e9519b37bd553
albamhp <albaherrerapalacio@gmail.com>
2019-03-18 19:01:36 +0100

    sthg

--- src/operations/__init__.py
+++ src/operations/__init__.py
@@ -2,3 +2,4 @@ from .sort import Sort
 from .kalman_tracking import KalmanTracking
 from .overlap_tracking import OverlapTracking
 from .siamese_tracking import SiameseTracking
+from .sort import associate_detections_to_trackers

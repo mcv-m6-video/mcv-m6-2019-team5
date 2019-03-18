5de7a94e990e9c818e8e4179288590116696e0d1
Jorge López <jorge.lopez@neosentec.com>
2019-03-18 16:29:39 +0100

    Minor changes

--- src/operations/kalman_tracking.py
+++ src/operations/kalman_tracking.py
@@ -1,7 +1,16 @@
+from model import Frame
+from operations import Sort
+import numpy as np
+
+
 class KalmanTracking:
 
     def __init__(self):
-        pass
+        self.mot_tracker = Sort()  # create instance of the SORT tracker
 
-    def __call__(self):
-        pass
+    def __call__(self, frame: Frame):
+        dets = frame.get_format_detections()
+        dets = np.array(dets)
+        trackers = self.mot_tracker.update(dets)
+
+        return trackers

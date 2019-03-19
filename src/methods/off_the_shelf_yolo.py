42593349757013f8650e5a2e6d66ea3ab62d4282
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-03-19 11:24:24 +0100

    Improved yolo and added method for ssd

--- src/methods/off_the_shelf_yolo.py
+++ src/methods/off_the_shelf_yolo.py
@@ -4,14 +4,14 @@ import torch
 from matplotlib import patches
 from torchvision import transforms
 
-from model import Video
+from model import Video, Detection, Frame
 from utils import utils
 from yolo.models import Darknet
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def off_the_shelf_yolo():
+def off_the_shelf_yolo(debug=False):
     video = Video("../datasets/AICity_data/train/S03/c010/frames")
     trans = transforms.Compose([
         transforms.Resize((416, 416)),
@@ -26,7 +26,7 @@ def off_the_shelf_yolo():
 
     model.eval()
     with torch.no_grad():
-        for im in video.get_frames():
+        for i, im in enumerate(video.get_frames()):
             im_tensor = trans(im)
             im_tensor = im_tensor.view((-1,) + im_tensor.size())
             if torch.cuda.is_available():
@@ -39,21 +39,27 @@ def off_the_shelf_yolo():
             scalex = im.width / 416
             scaley = im.height / 416
 
-            print(detections)
+            frame = Frame(i)
 
-            plt.figure()
+            if debug:
+                plt.figure()
 
             for d in detections[0]:
                 bbox = d.cpu().numpy()
                 x1 = int(scalex * bbox[0])
                 y1 = int(scaley * bbox[1])
-                rect = patches.Rectangle((x1, y1), scalex * (bbox[2] - bbox[0]), scaley * (bbox[3] - bbox[1]),
-                                         linewidth=2, edgecolor='blue', facecolor='none')
-                plt.gca().add_patch(rect)
-                plt.text(x1, y1, s=classes[int(d[6])],
-                         color='white', verticalalignment='top',
-                         bbox={'color': 'blue', 'pad': 0})
+                det = Detection(-1, classes[int(d[6])], (x1, y1), width=scalex * (bbox[2] - bbox[0]),
+                                height=scaley * (bbox[3] - bbox[1]), confidence=d[5])
+                frame.detections.append(det)
+                if debug:
+                    rect = patches.Rectangle((x1, y1), det.width, det.height,
+                                             linewidth=2, edgecolor='blue', facecolor='none')
+                    plt.gca().add_patch(rect)
+                    plt.text(x1, y1, s=det.label,
+                             color='white', verticalalignment='top',
+                             bbox={'color': 'blue', 'pad': 0})
 
-            plt.imshow(im_show)
-            plt.show()
-            plt.close()
+            if debug:
+                plt.imshow(im_show)
+                plt.show()
+                plt.close()

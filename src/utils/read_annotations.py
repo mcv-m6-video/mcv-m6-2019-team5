8256ca1dce607f2a7bb966c2b5a1398131515eb6
nilaisr <nilaisr@gmail.com>
2019-03-19 12:15:28 +0100

    read all annotations

--- src/utils/read_annotations.py
+++ src/utils/read_annotations.py
@@ -5,26 +5,25 @@ from typing import List
 from model import Detection
 
 
-def read_annotations(root_directory: str, start: int, end: int) -> List[List[Detection]]:
+def read_annotations(file_path: str) -> List[List[Detection]]:
     frames_detections = []
 
-    for i in range(start, end + 1):
-        frame_path = 'frame_{:04d}.xml'.format(i)
-        root = ET.parse(os.path.join(root_directory, frame_path)).getroot()
+    root = ET.parse(file_path).getroot()
 
-        frame_detections = []
+    frame_detections = []
 
-        for obj in root.findall('object'):
-            box = obj.find('bndbox')
+    for obj in root.findall('object'):
+        box = obj.find('bndbox')
 
-            label = obj.find('name').text
-            xmin = int(box.find('xmin').text)
-            ymin = int(box.find('ymin').text)
-            xmax = int(box.find('xmax').text)
-            ymax = int(box.find('ymax').text)
+        label = obj.find('name').text
+        xmin = int(box.find('xmin').text)
+        ymin = int(box.find('ymin').text)
+        xmax = int(box.find('xmax').text)
+        ymax = int(box.find('ymax').text)
 
-            frame_detections.append(Detection('', label, (xmin, ymin), xmax - xmin + 1, ymax - ymin + 1))
+        frame_detections.append(Detection('', label, (xmin, ymin), xmax - xmin + 1, ymax - ymin + 1))
 
-        frames_detections.append(frame_detections)
+    frames_detections.append(frame_detections)
 
-    return frames_detections
+
+return frames_detections

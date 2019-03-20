8ee9bacd44ad9f33e13cd63f955c1dc0800f14a7
Jorge Lopez <jorgelopezfueyo@gmail.com>
2019-03-20 14:49:14 +0100

    MInor changes in job

--- job_fine_tune_yolo.sh
+++ job_fine_tune_yolo.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH --job-name fcn8_camvid
+#SBATCH --job-name yolo
 #SBATCH --ntasks 4
 #SBATCH --mem 16G
-#SBATCH --qos masterhigh
 #SBATCH --partition mhigh,mlow
 #SBATCH --gres gpu:1
 #SBATCH --chdir /home/grupo06/mcv-m6-2019-team5

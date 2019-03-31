123d285a23ed11c3a00023675e4384cf19a8865a
albamhp <albaherrerapalacio@gmail.com>
2019-03-31 22:33:03 +0200

    some changes

--- src/optical_flow/horn_schunck.py
+++ src/optical_flow/horn_schunck.py
@@ -1,8 +1,8 @@
 import numpy as np
-#import bob.ip.optflow.hornschunck
 
 
 def horn_schunck(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
+    import bob.ip.optflow.hornschunck
     # flow = bob.ip.optflow.hornschunck.VanillaFlow(im1.shape) # GRAY
     flow = bob.ip.optflow.hornschunck.Flow(im1.shape)
     u, v = flow.estimate(alpha=200, iterations=20, image1=im1, image2=im2)

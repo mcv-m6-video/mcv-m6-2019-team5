bbcb53f305418a9e525e775992490b608e4d6678
Jorge Lopez <jorgelopezfueyo@gmail.com>
2019-03-31 19:36:47 +0200

    Minor fixes block

--- src/optical_flow/block_matching.py
+++ src/optical_flow/block_matching.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 class BlockMatching:
 
-    def __init__(self, block_size=8, window_size=64, stride=4, window_stride=4, criteria="SAD"):
+    def __init__(self, block_size=8, window_size=32, stride=4, window_stride=2, criteria="SAD"):
         self.block_size = block_size
         self.window_size = window_size
         self.stride = stride
@@ -25,39 +25,37 @@ class BlockMatching:
         out = np.zeros((im1.shape[0], im1.shape[1], 2))
         self.img_shape = im1.shape
 
-        w = ((self.img_shape[0] - self.window_size // 2) - self.window_size // 2) // self.stride
-        h = ((self.img_shape[1] - self.window_size // 2) - self.window_size // 2) // self.stride
+        h = ((self.img_shape[0] - self.window_size // 2) - self.window_size // 2) // self.stride
+        w = ((self.img_shape[1] - self.window_size // 2) - self.window_size // 2) // self.stride
         total = w * h
 
         # No padding used, if needed, we could use np.pad
-        for i, j in tqdm(itertools.product(
+        for j, i in tqdm(itertools.product(
                 range(self.window_size // 2, self.img_shape[0] - self.window_size // 2, self.stride),
                 range(self.window_size // 2, self.img_shape[1] - self.window_size // 2, self.stride)
         ), total=total, file=sys.stdout):
-            block = im1[i - self.block_size // 2:i + self.block_size // 2,
-                    j - self.block_size // 2:j + self.block_size // 2, :]
+            block = im1[j - self.block_size // 2:j + self.block_size // 2,
+                        i - self.block_size // 2:i + self.block_size // 2, :]
 
-            out[i, j, :] = self._find_maximum_matching(block, im2, (i, j))
+            out[j, i, :] = self._find_maximum_matching(block, im2, (j, i))
         return out
 
     def _find_maximum_matching(self, box1: np.ndarray, im2: np.ndarray, pixel1: tuple) -> tuple:
         min_likelihood = float('inf')
         min_direction = (0, 0)
-        for col, row in itertools.product(
-                range(- (self.window_size // 2) + (self.block_size // 2),
-                      (self.window_size // 2) - (self.block_size // 2), self.window_stride),
-                range(- (self.window_size // 2) + (self.block_size // 2),
-                      (self.window_size // 2) - (self.block_size // 2), self.window_stride)
-        ):
-            box2 = im2[pixel1[0] + col - self.block_size // 2:pixel1[0] + col + self.block_size // 2,
-                       pixel1[1] + row - self.block_size // 2:pixel1[1] + row + self.block_size // 2, :]
+
+        window_range = range(- (self.window_size // 2) + (self.block_size // 2),
+                             (self.window_size // 2) - (self.block_size // 2), self.window_stride)
+
+        for j, i in itertools.product(window_range, window_range):
+            box2 = im2[pixel1[0] + j - self.block_size // 2:pixel1[0] + j + self.block_size // 2,
+                       pixel1[1] + i - self.block_size // 2:pixel1[1] + i + self.block_size // 2, :]
             likelihood = self.criteria(box1, box2)
             if likelihood < min_likelihood:
                 min_likelihood = likelihood
-                min_direction = (col, row)
-            elif likelihood == min_likelihood and np.sum(np.power(min_direction, 2)) > col**2 + row**2:
-                min_direction = (col, row)
-
+                min_direction = (i, -j)
+            elif likelihood == min_likelihood and np.sum(np.power(min_direction, 2)) > j ** 2 + i ** 2:
+                min_direction = (i, -j)
 
         return min_direction
 

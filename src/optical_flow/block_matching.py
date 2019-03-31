6edfaa44aa4745c591ee83435de05ea557d2688d
Jorge Lopez <jorgelopezfueyo@gmail.com>
2019-03-31 18:08:18 +0200

    Using stride and progress bar

--- src/optical_flow/block_matching.py
+++ src/optical_flow/block_matching.py
@@ -1,20 +1,22 @@
+import itertools
+import sys
+
 import cv2
 import numpy as np
-from math import pow
+from tqdm import tqdm
 
 
 class BlockMatching:
 
-    def __init__(self, block_size=8, window_size=64, stride=4, criteria="SAD"):
+    def __init__(self, block_size=8, window_size=64, stride=4, window_stride=4, criteria="SAD"):
         self.block_size = block_size
         self.window_size = window_size
         self.stride = stride
+        self.window_stride = window_stride
 
         criteria_refs = {
             'SAD': self._sum_absolute_differences,
             'SSD': self._sum_square_differences,
-            'MAE': self._mean_absolute_error,
-            'MSE': self._mean_square_error
         }
 
         self.criteria = criteria_refs.get(criteria)
@@ -22,30 +24,37 @@ class BlockMatching:
     def __call__(self, im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
         out = np.zeros((im1.shape[0], im1.shape[1], 2))
         self.img_shape = im1.shape
-        # No padding used, if needed, we could use np.pad
-        for i in range(self.window_size // 2, self.img_shape[0] - self.window_size // 2):
-            for j in range(self.window_size // 2, self.img_shape[1] - self.window_size // 2):
-                block = im1[i - self.block_size // 2:i + self.block_size // 2,
-                        j - self.block_size // 2:j + self.block_size // 2, :]
-                maximum_matching = self._find_maximum_matching(block, im2, (i, j))
 
-                out[i, j, :] = maximum_matching
+        w = ((self.img_shape[0] - self.window_size // 2) - self.window_size // 2) // self.stride
+        h = ((self.img_shape[1] - self.window_size // 2) - self.window_size // 2) // self.stride
+        total = w * h
+
+        # No padding used, if needed, we could use np.pad
+        for i, j in tqdm(itertools.product(
+                range(self.window_size // 2, self.img_shape[0] - self.window_size // 2, self.stride),
+                range(self.window_size // 2, self.img_shape[1] - self.window_size // 2, self.stride)
+        ), total=total, file=sys.stdout):
+            block = im1[i - self.block_size // 2:i + self.block_size // 2,
+                    j - self.block_size // 2:j + self.block_size // 2, :]
+            maximum_matching = self._find_maximum_matching(block, im2, (i, j))
+
+            out[i, j, :] = maximum_matching
         return out
 
     def _find_maximum_matching(self, box1: np.ndarray, im2: np.ndarray, pixel1: tuple) -> tuple:
-        likelihood_aux = 0
-        out = (0, 0)
+        max_likelihood = 0
+        max_direction = (0, 0)
         for col in range(pixel1[0] - (self.window_size // 2) + (self.block_size // 2),
-                         pixel1[0] + (self.window_size // 2) - (self.block_size // 2)):
+                         pixel1[0] + (self.window_size // 2) - (self.block_size // 2), self.window_stride):
             for row in range(pixel1[1] - (self.window_size // 2) + (self.block_size // 2),
-                             pixel1[1] + (self.window_size // 2) - (self.block_size // 2)):
-                box2 = im2[col - self.block_size // 2:col + self.block_size // 2, row - self.block_size //
-                                                                                  2:row + self.block_size // 2, :]
+                             pixel1[1] + (self.window_size // 2) - (self.block_size // 2), self.window_stride):
+                box2 = im2[col - self.block_size // 2:col + self.block_size // 2,
+                       row - self.block_size // 2:row + self.block_size // 2, :]
                 likelihood = self.criteria(box1, box2)
-                if likelihood > likelihood_aux:
-                    likelihood_aux = likelihood
-                    out = (col, row)
-        return out
+                if likelihood > max_likelihood:
+                    max_likelihood = likelihood
+                    max_direction = (col, row)
+        return max_direction
 
     @staticmethod
     def _sum_absolute_differences(box1: np.ndarray, box2: np.ndarray) -> float:
@@ -54,14 +63,6 @@ class BlockMatching:
     @staticmethod
     def _sum_square_differences(box1: np.ndarray, box2: np.ndarray) -> float:
         return float(np.sum(np.power(box1 - box2, 2)))
-
-    @staticmethod
-    def _mean_absolute_error(self, box1: np.ndarray, box2: np.ndarray) -> float:
-        return float(np.sum(np.abs(box1 - box2)) / pow(self.block_size, 2))
-
-    @staticmethod
-    def _mean_square_error(self, box1: np.ndarray, box2: np.ndarray) -> float:
-        return float(np.sum(np.power(box1 - box2, 2)) / pow(self.block_size, 2))
 
 
 if __name__ == '__main__':

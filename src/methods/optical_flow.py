b2094293dab0df671f95eae94969c44d7f58b495
albamhp <albaherrerapalacio@gmail.com>
2019-04-01 12:03:45 +0200

    gunner

--- src/methods/optical_flow.py
+++ src/methods/optical_flow.py
@@ -12,9 +12,9 @@ def optical_flow(optical_flow_method, debug: bool = False, **kwargs):
     :param debug: whether to show debug plots
     """
 
-    im1 = cv2.imread('../datasets/optical_flow/img/000045_10.png')
-    im2 = cv2.imread('../datasets/optical_flow/img/000045_11.png')
-    gt = read_optical_flow('../datasets/optical_flow/gt/000045_10.png')
+    im1 = cv2.imread('../datasets/optical_flow/img/000157_10.png')  # 045
+    im2 = cv2.imread('../datasets/optical_flow/img/000157_11.png')
+    gt = read_optical_flow('../datasets/optical_flow/gt/000157_10.png')
 
     flow = optical_flow_method(im1, im2)
 

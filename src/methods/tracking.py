f582635db795235cdc2551b727248b8281c484c6
nilaisr <nilaisr@gmail.com>
2019-04-02 18:53:54 +0200

    compute map gt det

--- src/methods/tracking.py
+++ src/methods/tracking.py
@@ -1,11 +1,10 @@
 import sys
 
 from tqdm import tqdm
-import motmetrics as mm
 
 from model import Video
 from utils import read_detections, read_annotations
-from metrics import idf1
+from metrics import Mot
 
 START = 500
 STRIDE = 1
@@ -15,7 +14,9 @@ def tracking(optical_flow_method, tracking_method, debug, **kwargs):
     video = Video('../datasets/AICity_data/train/S03/c010/frames')
     detections = read_detections('../datasets/AICity_data/train/S03/c010/det/det_yolo3.txt')
     gt = read_annotations('../datasets/AICity_data/train/S03/c010/m6-full_annotation.xml')
-    acc = mm.MOTAccumulator(auto_id=True)
+
+    mot = Mot()
+
     previous_frame = None
     for i, frame in tqdm(enumerate(video.get_frames(start=START)), total=len(video) - START, file=sys.stdout):
         if i == 0:
@@ -25,9 +26,9 @@ def tracking(optical_flow_method, tracking_method, debug, **kwargs):
             tracking_method(optical_flow_method, previous_frame, detections[i - STRIDE + START], frame,
                             detections[i + START], debug)
             previous_frame = frame
+        mot.compute_idf1(detections[i], gt[i])
 
-    # idf1(detections[i], gt[i], acc)
-    if not debug:
-        mh = mm.metrics.create()
-        summary = mh.compute(acc, metrics=mm.metrics.motchallenge_metrics, name='acc')
-        print(summary)
+    idf1 = mot.get_idf1()
+    print(idf1)
+    a = mot.get_events()
+    print(a)

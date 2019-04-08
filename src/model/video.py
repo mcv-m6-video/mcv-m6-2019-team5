243e6ec65bb57c1cd80ed43cee36ef40085cf1b9
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-04-08 12:45:01 +0200

    FIX VIdeo

--- src/model/video.py
+++ src/model/video.py
@@ -1,21 +1,19 @@
 import os
-from typing import Iterator
-
-from PIL import Image
-from torch.utils.data import Dataset
 import cv2
 
 from utils import read_detections
 
 
-class Video(Dataset):
+class Video():
 
     def __init__(self, video_path: str):
         self.video_path = video_path
 
     def get_frames(self):
         video = cv2.VideoCapture(os.path.join(self.video_path, "vdo.avi"))
-        full_detections = read_detections(os.path.join(self.video_path, "det/"))
+        full_detections = read_detections(os.path.join(self.video_path, "det/det_yolo3.txt"))
+        full_GT = read_detections(os.path.join(self.video_path, "gt/gt.txt"))
+
         count = 0
         while video.isOpened():
             ret, frame = video.read()

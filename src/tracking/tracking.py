33537be478760c8cd3eb91150626447cbd84f7af
albamhp <albaherrerapalacio@gmail.com>
2019-04-08 13:14:36 +0200

    Fix track_detections

--- src/tracking/tracking.py
+++ src/tracking/tracking.py
@@ -22,7 +22,7 @@ class KalmanTracking:
             # print(match[0], " . ", match[1], " . ", frame.detections[match[0]].top_left, " . ", trackers[match[1]])
 
         if debug:
-            grid = np.array(self.video[frame.id])
+            grid = np.array(frame.image)
             for d in frame.detections:
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 cv2.putText(grid, str(d.id), (int(d.top_left[1]), int(d.top_left[0])), font, 1, (255, 255, 255), 2,
@@ -36,9 +36,7 @@ class KalmanTracking:
 def track_detections():
 
     kalman = KalmanTracking()
-    for video in Sequence("../datasets/AICity_data/train/S03"):
-        for frame in video:
-
+    for video in Sequence("../datasets/AICity_data/train/S03").get_videos():
+        for frame in video.get_frames():
             kalman(frame)
             print(seq(frame.detections).map(lambda d: d.id).to_list())
-

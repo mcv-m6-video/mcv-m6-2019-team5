833da4be75b77ad5a5854397ef271eb97c414e7f
albamhp <albaherrerapalacio@gmail.com>
2019-04-08 12:37:44 +0200

    tracking fixed2

--- src/tracking/__init__.py
+++ src/tracking/__init__.py
@@ -1,2 +1,4 @@
-from .sort import Sort, associate_detections_to_trackers
-from .tracking import KalmanTracking
+from .sort import Sort
+from .tracking import KalmanTracking, track_detections
+from .kalman_tracking import KalmanBoxTracker
+from .data_association import associate_detections_to_trackers

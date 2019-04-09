2f8e16fd7042c2da336bb432ebc8596efcf0549c
Jorge López <jorge.lopez@neosentec.com>
2019-04-09 20:30:22 +0200

    Configuration for each sequence

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 
 from functional import seq
 
@@ -8,14 +9,20 @@ from tracking import KalmanTracking
 
 def main():
     parser = argparse.ArgumentParser(description='Week 5 M6')
-
+    parser.add_argument('sequence', type=str, choices=('train_seq1', 'train_seq3', 'train_seq4'), default='train_seq3')
+    parser.add_argument('tracking_method', type=str, choices=('kalman', 'overlap', 'optical_flow'), default='kalman')
     parser.add_argument('-d', '--debug', action='store_true', help='Show debug plots')
     args = parser.parse_args()
 
-    kalman = KalmanTracking()
-    for video in Sequence("../datasets/AICity_data/train/S03").get_videos():
+    config = configparser.ConfigParser()
+    config.read('configuration/config.ini')
+
+    if args.tracking_method is 'kalman':
+        kalman = KalmanTracking()
+    for video in Sequence(config.get(args.sequence, 'sequence_path')).get_videos():
         for frame in video.get_frames():
-            kalman(frame, args.debug)
+            if args.tracking_method is 'kalman':
+                kalman(frame, args.debug)
             print(seq(frame.detections).map(lambda d: d.id).to_list())
 
 
--- src/model/siamese_db.py
+++ src/model/siamese_db.py
@@ -4,3 +4,6 @@ import numpy as np
 class SiameseDB:
     def __init__(self, dimensions: int):
         self.db = np.empty((0, dimensions))
+
+    def __call__(self, *args, **kwargs):
+        pass

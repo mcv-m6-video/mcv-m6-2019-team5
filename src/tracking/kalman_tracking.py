6c0ae15769e9825eb90334315384c75469c3642c
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-04-10 23:10:29 +0200

    ADD Bounding Box Colorcitos

--- src/tracking/kalman_tracking.py
+++ src/tracking/kalman_tracking.py
@@ -7,13 +7,15 @@ from matplotlib import patches
 from model import Frame, SiameseDB
 from .kalman import Sort, associate_detections_to_trackers
 from utils import IDGenerator
-
+from matplotlib import colors
 class KalmanTracking:
 
     def __init__(self):
         self.mot_tracker = Sort()  # create instance of the SORT tracker
+        viridis = colors.ListedColormap(np.random.rand(256, 3))
+        self.new_color = viridis(np.linspace(0, 1, 256))
 
-    def __call__(self, frame: Frame, siamese: SiameseDB, debug=False):
+    def __call__(self, frame: Frame, siamese: SiameseDB, debug=False, plot_number=False):
 
         detections = seq(frame.detections).map(lambda d: d.to_sort_format()).to_list()
         detections = np.array(detections)
@@ -33,7 +35,26 @@ class KalmanTracking:
                 frame.detections[unmatched].id = IDGenerator.next()
 
         if debug:
-            self.plot_tracking(frame)
+            self.plot_tracking_color(frame, plot_number)
+
+    def plot_tracking_color(self, frame: Frame, plot_number):
+        plt.imshow(cv2.cvtColor(frame.image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+
+        for det in frame.detections:
+            if det.id != -1:
+                rect = patches.Rectangle((det.top_left[0], det.top_left[1]), det.width, det.height,
+                                         linewidth=2, edgecolor=self.new_color[det.id, :], facecolor='none')
+                plt.gca().add_patch(rect)
+                if plot_number:
+                    plt.text(det.top_left[0] - 0, det.top_left[1] - 50, s='{}'.format(det.id),
+                             color='white', verticalalignment='top',
+                             bbox={'color': 'blue', 'pad': 0})
+                plt.gca().add_patch(rect)
+        plt.imshow(cv2.cvtColor(frame.image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        plt.show()
+        plt.close()
 
     @staticmethod
     def plot_tracking(frame: Frame):
--- src/tracking/overlap_tracking.py
+++ src/tracking/overlap_tracking.py
@@ -1,9 +1,10 @@
 import cv2
 import matplotlib.pyplot as plt
-from matplotlib import patches
+from matplotlib import patches, colors
 
 from model import Frame, Detection, SiameseDB
 from utils import IDGenerator
+import numpy as np
 
 INTERSECTION_THRESHOLD = 0.55
 
@@ -16,8 +17,10 @@ class OverlapTracking:
     def __init__(self, look_back=3):
         self.look_back = look_back
         self.prev_det = None
+        viridis = colors.ListedColormap(np.random.rand(256, 3))
+        self.new_color = viridis(np.linspace(0, 1, 256))
 
-    def __call__(self, frame: Frame, siamese: SiameseDB, debug=False) -> None:
+    def __call__(self, frame: Frame, siamese: SiameseDB, debug=False, plot_number=False) -> None:
         for detection in frame.detections:
             self._find_id(detection)
             if detection.id == -1:
@@ -32,7 +35,7 @@ class OverlapTracking:
         self.prev_det = frame.detections
 
         if debug:
-            self.plot_tracking(frame)
+            self.plot_tracking_color(frame, plot_number)
 
     @staticmethod
     def plot_tracking(frame: Frame):
@@ -47,6 +50,26 @@ class OverlapTracking:
                      color='white', verticalalignment='top',
                      bbox={'color': 'blue', 'pad': 0})
             plt.gca().add_patch(rect)
+        plt.imshow(cv2.cvtColor(frame.image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        plt.show()
+        plt.close()
+
+    def plot_tracking_color(self, frame: Frame, plot_number):
+        plt.imshow(cv2.cvtColor(frame.image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+
+        for det in frame.detections:
+            if det.id != -1:
+                rect = patches.Rectangle((det.top_left[0], det.top_left[1]), det.width, det.height,
+                                         linewidth=2, edgecolor=self.new_color[det.id, :], facecolor='none')
+                plt.gca().add_patch(rect)
+                if plot_number:
+                    plt.text(det.top_left[0] - 0, det.top_left[1] - 50, s='{}'.format(det.id),
+                             color='white', verticalalignment='top',
+                             bbox={'color': 'blue', 'pad': 0})
+
+                plt.gca().add_patch(rect)
         plt.imshow(cv2.cvtColor(frame.image, cv2.COLOR_BGR2RGB))
         plt.axis('off')
         plt.show()

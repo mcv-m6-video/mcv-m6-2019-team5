7c5f46d08acc0c5199319c579e6a349575d4404b
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-04-11 12:21:58 +0200

    ADD Write detections

--- src/utils/write_detections.py
+++ src/utils/write_detections.py
@@ -4,4 +4,13 @@ from model import Detection
 
 
 def write_detections(det: List[List[Detection]]):
-    pass
+    file = open("detections.txt", "w")
+    frame_id = 0
+    for det1 in det:
+        frame_id += 1
+        for det2 in det1:
+            out = str(frame_id) + "," + str(det2.id) + "," + str(det2.top_left[0]) + "," + str(
+                det2.top_left[1]) + "," + str(det2.width) + "," + str(det2.height) + "," + str(
+                det2.confidence) + "," + str(-1) + "," + str(-1) + "," + str(-1)
+            file.write(out)
+    file.close()
